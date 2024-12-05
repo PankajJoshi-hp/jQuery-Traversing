9d5e2e262be0bd20856ac697858504e6f60420e3
Pankaj Joshi <Pankaj.Joshi.hp@gmail.com>
2024-12-05 16:27:47 +0530

    second commit

--- script.js
+++ script.js
@@ -16,4 +16,35 @@ jQuery(function () {
 
     parentLi.find("input.parent-checkbox").prop("checked", allUnchecked);
   });
+
+  $("a").on("click", function (e) {
+    e.preventDefault();
+
+    const addNew = `
+     <div class="form-row">
+        <div class="left-side">
+          <label class="label-two" for="property-image">Property Image</label>
+          <input type="file" id="property-image" />
+        </div>
+        <div class="right-side">
+          <select class="select">
+            <option value="primary">Primary Image</option>
+            <option value="secondary">Secondary Image</option>
+          </select>
+          <button class="remove-btn">Remove</button>
+        </div>
+      </div>`;
+
+    $(".form-container").append(addNew);
+  });
+
+  $(document).on("click", ".remove-btn", function () {
+    $(this).closest(".form-row").remove();
+  });
+
+  $(document).on("change", ".select", function () {
+    if ($(this).val() === "primary") {
+      $(".select").not(this).val("secondary");
+    }
+  });
 });
